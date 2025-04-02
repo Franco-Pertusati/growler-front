ff46a87fc519c52af666e928d624c481d13eec1a
Franco Pertusati <pertusatifranco836@gmail.com>
2025-04-02 20:25:55 -0300

    Angular material fixed + cambio de layout

--- src/app/pages/inventory/categories/categories.component.ts
+++ src/app/pages/inventory/categories/categories.component.ts
@@ -1,25 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ApiService } from '../../../services/api.service';
-import { Product } from '../../../modules/products';
-import { CreateCategoryFormComponent } from "./forms/create-category-form/create-category-form.component";
-import { CreateProductFormComponent } from "./forms/create-product-form/create-product-form.component";
 import { ButtonComponent } from "../../../ui/button/button.component";
 import { CommonModule } from '@angular/common';
 import { InputContainerComponent } from "../../../ui/input-container/input-container.component";
 import { DropdownComponent } from "../../../ui/dropdown/dropdown.component";
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
+import { CreateCategoryFormComponent } from './forms/create-category-form/create-category-form.component';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [CreateCategoryFormComponent, CreateProductFormComponent, ButtonComponent, CommonModule, InputContainerComponent, DropdownComponent],
+  imports: [ButtonComponent, CommonModule, InputContainerComponent, DropdownComponent, DialogModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
   categories: any[] = []
+  dialog = inject(Dialog);
 
   constructor(private apiService: ApiService) { }
 
+  openDialog() {
+    const dialogRef =
+    this.dialog.open<string>(CreateCategoryFormComponent)
+  }
 
   ngOnInit(): void {
     this.loadCategories();
@@ -36,5 +40,6 @@ export class CategoriesComponent {
       }
     );
   }
+
 
 }
---
+++ src/app/ui/acordeon/acordeon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acordeon',
+  standalone: true,
+  imports: [],
+  templateUrl: './acordeon.component.html',
+  styleUrl: './acordeon.component.css'
+})
+export class AcordeonComponent {
+
+}
